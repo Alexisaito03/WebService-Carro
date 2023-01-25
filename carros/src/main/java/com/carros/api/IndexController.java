6c1d0462e78ffb022688b266bcc9c853a0f9839c
INDRA\aken <aken@minsait.com>
2023-01-25 18:00:55 -0300

    Correção e criação de métodos e classes

---
+++ carros/src/main/java/com/carros/api/IndexController.java
@@ -0,0 +1,12 @@
+package com.carros.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    public String get(){
+        return "API dos carros";
+    }
+}
