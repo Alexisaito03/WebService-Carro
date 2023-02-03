0326c90197164b5e74b865287785dfbace3d36a3
Alexisaito03 <alexi_k_sr@hotmail.com>
2023-02-03 11:42:01 -0300

    Criação do objeto e referência com o banco MySQL

--- carros/src/main/java/com/carros/api/IndexController.java
+++ carros/src/main/java/com/carros/api/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class IndexController {
-    public String get(){
+    public String get() {
         return "API dos carros";
     }
 }
--- carros/src/main/java/com/carros/domain/Carro.java
+++ carros/src/main/java/com/carros/domain/Carro.java
@@ -1,8 +1,20 @@
 package com.carros.domain;
 
+import javax.persistence.*;
+
+@Entity(name = "carro") //O name só é necessário caso a classe possua o nome diferente da tabela no banco
 public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Gerar um Id autoincrement automaticamente
     private Long id;
+
+    @Column(name = "nome") // Seria necessário caso o nome do atributo fosse diferente do nome da coluna da tabela
     private String nome;
+
+    public Carro(){
+
+    }
 
     public Carro(Long id, String nome) {
         this.id = id;
